057229c520163f4b924e82dee05e5cbe41ff35a2
Bryan Travis Smith <bryantravissmith@Bryans-MacBook-Pro.local>
2017-06-21 19:55:13 -0700

    make blog

---
+++ content/pages/about.md
@@ -0,0 +1,8 @@
+Title: About
+Date: 2017-07-01 9:34
+
+This site is a collections of topics that am or have attempted to learn.   It
+has been my experience that I learn best by trying to explain it to someone
+else.   That really makes this blog more about me, but I sincerely hope that
+you find something useful here.   Feel free to reach out if you can think of
+something you'd like to me write about, fix, or correct
---
+++ pelicanconf.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = 'Bryan Travis Smith, Ph.D'
+SITENAME = 'Bryan Travis Smith, Ph.D'
+SITEURL = 'http://bryantravissmith.com'
+
+PATH = 'content'
+# Useful for Development
+RELATIVE_URLS = True
+
+TIMEZONE = 'America/Los_Angeles'
+
+DEFAULT_LANG = 'en'
+
+# Theme
+THEME = 'theme/'
+
+# Formt of urls
+ARTICLE_URL = '{category}/{slug}/'
+ARTICLE_SAVE_AS = '{category}/{slug}/index.html'
+PAGE_URL = '{slug}/'
+PAGE_SAVE_AS = '{slug}/index.html'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (('Pelican', 'http://getpelican.com/'),
+         ('Python.org', 'http://python.org/'),
+         ('Jinja2', 'http://jinja.pocoo.org/'),
+         ('You can modify those links in your config file', '#'),)
+
+# Social widget
+SOCIAL = (('You can add links in your config file', '#'),
+          ('Another social link', '#'),)
+
+DEFAULT_PAGINATION = 10
+
+# Added for Jupyter Notebook integration
+# https://github.com/danielfrg/pelican-ipynb
+
+MARKUP = ('md', 'ipynb')
+
+PLUGIN_PATHS = ['./plugins']
+PLUGINS = ['ipynb.markup']
+
+IGNORE_FILES = ['.ipynb_checkpoints']
+IPYNB_USE_META_SUMMARY = True
+
+# Set Defaults
+
+DISPLAY_PAGES_ON_MENU = True
+
+# Uncomment following line if you want document-relative URLs when developing
+# RELATIVE_URLS = True
